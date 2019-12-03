aa0a71a7e2ece87404e4d07a63288876518f8206
daniel.hs <daniel.hs@kakaocorp.com>
2019-12-03 09:11:43 +0900

    add Summernote custom widget

---
+++ src/components/summernote/modules/Hello.js
@@ -0,0 +1,16 @@
+import { Modal, Button } from 'antd';
+
+export const HelloButton = ($summernote, $) => context => {
+  const summernote = $.summernote;
+  const button = $.summernote.ui.button({
+    contents: '<i class="fa fa-child"/> Hello',
+    tooltip: "hello",
+    click: function() {
+      $summernote.summernote(
+        "pasteHTML",
+        "<img  src='https://t1.kakaocdn.net/kakaohairshop/assets/images/promotion/m640/191112_greathair/img_greathair_tab.png'/>"
+      );
+    }
+  });
+  return button.render();
+};
--- src/containers/SummernotePage.js
+++ src/containers/SummernotePage.js
@@ -1,18 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import { Modal, Button } from "antd";
+import { HelloButton } from "../components/summernote/modules/Hello";
+
+const SampleTemplate = () => {
+  return "hello";
+};
 
 const SummerNotePage = () => {
+  const [dialogShow, setDialogShow] = useState(false);
+
   useEffect(() => {
     const $ = window.$;
+    const $summernote = $(".summernote");
 
-    $(".summernote").summernote({
+    $summernote.summernote({
       height: 500,
+      toolbar: [["mybutton", ["hello"]]],
+      buttons: {
+        hello: HelloButton($summernote, $)
+      }
     });
 
     return () => {
-      $(".summernote").summernote("destroy");
+      $summernote.summernote("destroy");
     };
   }, []);
-  return <div className="summernote" />;
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            setDialogShow(true);
+          }}
+        >
+          hello
+        </button>
+      </div>
+      <div className="summernote" />
+      <Modal
+        visible={dialogShow}
+        onOk={() => {
+          window
+            .$(".summernote")
+            .summernote("pasteHTML", renderToStaticMarkup(<SampleTemplate />));
+          setDialogShow(false);
+        }}
+        onCancel={() => setDialogShow(false)}
+      />
+    </div>
+  );
 };
 
 export default SummerNotePage;
