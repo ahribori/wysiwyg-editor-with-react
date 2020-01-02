0b5e93a947c0a6984132527a74418412f0950c90
ahribori <ahribori@gmail.com>
2020-01-03 00:27:43 +0900

    CKEditor v5 test

--- src/containers/CKEditorPage.js
+++ src/containers/CKEditorPage.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import '../styles/ckeditor-content.css';
 
 const CKEditorPage = () => {
+
+  const [editor, setEditor] = useState(null);
+  const [userInputText, setUserInputText] = useState('');
+
+  useEffect(() => {
+    if (!editor) {
+      ClassicEditor.create(document.getElementById('editor'))
+        .then((editor) => {
+          console.log('CKEditor initialized')
+          setEditor(editor);
+        })
+    }
+
+    return () => {
+      if (editor) {
+        editor.destroy();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (editor) {
+      editor.model.document.on('change:data', () => {
+        console.log('The data has changed!');
+      });
+    }
+  }, [editor])
+
   return (
-    <div>
-      CKEDitor
-    </div>
+    <>
+      <div id="editor"/>
+      {
+        editor && (
+          <>
+            <input type="text" onChange={e => setUserInputText(e.target.value)} value={userInputText}/>
+            <button onClick={e => {
+              editor.setData(userInputText)
+            }}>setData
+            </button>
+            <button onClick={e => {
+
+            }}>현재 커서에 setData</button>
+          </>
+        )
+      }
+    </>
   );
 };
 
