e5f160a7ba923e92851e5626aff4b994428725ea
daniel.hs <daniel.hs@kakaocorp.com>
2020-01-07 10:10:33 +0900

    setData 하는 여러가지 방법

--- src/components/ckeditor5/Toolbar.js
+++ src/components/ckeditor5/Toolbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HtmlDataProcessor from "@ckeditor/ckeditor5-engine/src/dataprocessor/htmldataprocessor";
 
 const Toolbar = ({ editor }) => {
   return (
@@ -12,9 +13,46 @@ const Toolbar = ({ editor }) => {
           padding: 10
         }}
       >
-        <button onClick={() => {
-          console.log(editor.model)
-        }}>getData</button>
+        <button
+          onClick={() => {
+            console.log(editor.getData());
+          }}
+        >
+          getData
+        </button>
+
+        <button
+          onClick={() => {
+            const docFrag = editor.model.change(writer => {
+              const p1 = writer.createElement("paragraph");
+              const p2 = writer.createElement("paragraph");
+              const blockQuote = writer.createElement("blockQuote");
+              const docFrag = writer.createDocumentFragment();
+
+              writer.append(p1, docFrag);
+              writer.append(blockQuote, docFrag);
+              writer.append(p2, blockQuote);
+              writer.insertText("foo", p1);
+              writer.insertText("bar", p2);
+
+              return docFrag;
+            });
+            editor.model.insertContent(docFrag);
+          }}
+        >
+          docFrag insertContent
+        </button>
+
+        <button
+          onClick={() => {
+            const htmlDP = new HtmlDataProcessor();
+            const viewFragment = htmlDP.toView("<p>hello</p>");
+            const modelFragment = editor.data.toModel(viewFragment);
+            editor.model.insertContent(modelFragment);
+          }}
+        >
+          htmlString insertContent
+        </button>
       </div>
     )
   );
